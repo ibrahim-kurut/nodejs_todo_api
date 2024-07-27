a4fc491f34e78f62c029f7a831287cf99a765156
ibrahim kurut <ibrahim.kurut06@gmail.com>
2024-07-27 10:32:19 +0300

    Get user profile & validate Object Id

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -12,3 +12,17 @@ module.exports.getAllUsersCtrl = asyncHandler(async (req, res) => {
     const users = await User.find()
     res.status(200).json(users)
 })
+
+/**
+ * @desc Get user profile
+ * @route /api/users/profile/:id
+ * @method GET 
+ * @access public
+ */
+module.exports.getUserProfileCtrl = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select("-password")
+    if (!user) {
+        res.status(404).json({ message: "user not found" })
+    }
+    res.status(200).json(user)
+})
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,9 +1,12 @@
 const router = require("express").Router()
-const { getAllUsersCtrl } = require("../controllers/usersController")
+const { getAllUsersCtrl, getUserProfileCtrl } = require("../controllers/usersController")
+const validateObjId = require("../middlewares/validateObjId")
 const { verifyTokenAndAdmin } = require("../middlewares/verifyToken")
 
 
 // /api/users/profile
 router.route("/profile").get(verifyTokenAndAdmin, getAllUsersCtrl)
+// /api/users/profile/:id
+router.route("/profile/:id").get(validateObjId, getUserProfileCtrl)
 
 module.exports = router
