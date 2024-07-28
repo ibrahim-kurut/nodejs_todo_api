21446e7e7cca25f46405f93517e2adb00d53f4cc
ibrahim kurut <ibrahim.kurut06@gmail.com>
2024-07-28 14:28:03 +0300

    fix bugs in create todo

--- src/models/TodoModel.js
+++ src/models/TodoModel.js
@@ -23,7 +23,7 @@ const TodoSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true
+        require: true
     },
 
 }, { timestamps: true })
